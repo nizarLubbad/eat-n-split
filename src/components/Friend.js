993093ccac27057a268457b45c728adbf93aa1a1
Nizar Lubbad <nezar.b.lubbbad@gmail.com>
2025-08-11 17:56:13 +0300

    Add button and balance color styles; update Friend component to use them

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -4,19 +4,19 @@ export default function Friend({ friend }) {
       <img src={friend.image} alt={friend.name} />
       <h3>{friend.name}</h3>
       {friend.balance < 0 && (
-        <p>
+        <p className="red">
           You owe {friend.name} {Math.abs(friend.balance)}
         </p>
       )}
 
       {friend.balance > 0 && (
-        <p>
+        <p className="green">
           {friend.name} owes you {Math.abs(friend.balance)}
         </p>
       )}
 
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <button>Select</button>
+      <button className="button">Select</button>
     </li>
   );
 }
