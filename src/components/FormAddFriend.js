b4d647c075d4ab65d96ecc587b534a6a0c3a57a4
Nizar Lubbad <nezar.b.lubbbad@gmail.com>
2025-08-12 19:34:24 +0300

    Refine FormAddFriend labels and add form styling

--- src/components/FormAddFriend.js
+++ src/components/FormAddFriend.js
@@ -3,10 +3,10 @@ import Button from "./Button";
 export default function FormAddFriend() {
   return (
     <form className="form-add-friend">
-      <label>🧑‍🤝‍🧑 Friend Name: </label>
+      <label>🧍Friend name</label>
       <input type="text" />
 
-      <label>🖼️ Image URL: </label>
+      <label>🖼️ Image URL</label>
       <input type="text" />
 
       <Button>Add</Button>
