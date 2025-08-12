c6b0c422c57fef00d8620256104d014c225424c7
Nizar Lubbad <nezar.b.lubbbad@gmail.com>
2025-08-12 18:52:34 +0300

    Extract reusable Button component and use it in Friend

--- src/components/Friend.js
+++ src/components/Friend.js
@@ -1,3 +1,5 @@
+import Button from "./Button";
+
 export default function Friend({ friend }) {
   return (
     <li>
@@ -16,7 +18,7 @@ export default function Friend({ friend }) {
       )}
 
       {friend.balance === 0 && <p>You and {friend.name} are even</p>}
-      <button className="button">Select</button>
+      <Button>Select</Button>
     </li>
   );
 }
